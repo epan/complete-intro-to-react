296a28ccbb626a11456245477c5163a05d40bf9b
Eric Pan <epan@users.noreply.github.com>
2017-08-19 13:42:47 -0700

    (feat) Render show titles in a list

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,7 +3,7 @@ import preload from '../data.json';
 
 const Search = () => (
   <div className="search">
-    <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+    {preload.shows.map(show => <h3>{show.title}</h3>)}
   </div>
 );
 
