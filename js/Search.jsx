3d130cb6ce112a12ee42c0e14dc6cf56d04ed491
Eric Pan <epan@users.noreply.github.com>
2017-08-19 15:15:24 -0700

    (feat) Add bound input to state

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => (
-  <div className="search">
-    <header>
-      <h1>svideo</h1>
-      <input type="text" placeholder="Search" />
-    </header>
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
-    </div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input type="text" placeholder="Search" onChange={this.handleInput} name="searchTerm" />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
