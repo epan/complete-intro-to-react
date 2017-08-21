5f363f1590a6756ded07ba115d80f45f122a5178
Eric Pan <epan@users.noreply.github.com>
2017-08-21 01:31:03 -0700

    (test) Add test based on change event in search

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -14,4 +14,12 @@ test('Search should render correct amount of shows', () => {
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
-xtest('Search should render correct amount of shows', () => {});
+test('Search should render correct amount of shows based on search term', () => {
+  const searchTerm = 'black';
+  const component = shallow(<Search />);
+  component.find('input').simulate('change', { target: { value: searchTerm } });
+  const showCount = preload.shows.filter(
+    show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchTerm.toUpperCase()) >= 0
+  ).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
+});
