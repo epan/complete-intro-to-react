9eb6cf181419f968f229c136235818a3af30dc98
Eric Pan <epan@users.noreply.github.com>
2017-08-31 22:42:26 -0700

    not really working flow

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
@@ -6,8 +8,8 @@ class Search extends Component {
   state = {
     searchTerm: ''
   };
-
-  handleInput = e => {
+  
+  handleInput = (e: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
     this.setState({ searchTerm: e.target.value });
   };
 
@@ -16,13 +18,20 @@ class Search extends Component {
       <div className="search">
         <header>
           <h1>{this.state.searchTerm}</h1>
-          <input type="text" placeholder="Search" onChange={this.handleInput} name="searchTerm" />
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={this.handleInput}
+            name="searchTerm"
+          />
         </header>
         <div>
           {preload.shows
             .filter(
               show =>
-                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
             )
             .map(show => <ShowCard key={show.imdbID} show={show} />)}
         </div>
