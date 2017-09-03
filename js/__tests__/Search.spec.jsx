0431682970ba0c6fb677843d79071633039f566a
Eric Pan <epan@users.noreply.github.com>
2017-09-03 00:15:36 -0700

    (feat) use header component

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -5,18 +5,18 @@ import Search from '../Search';
 import ShowCard from '../ShowCard';
 
 test('Search renders', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component).toMatchSnapshot();
 });
 
 test('Search should render correct amount of shows', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test('Search should render correct amount of shows based on search term', () => {
   const searchTerm = 'black';
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   component.find('input').simulate('change', { target: { value: searchTerm } });
   const showCount = preload.shows.filter(
     show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchTerm.toUpperCase()) >= 0
