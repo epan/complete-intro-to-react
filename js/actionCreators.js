3afd010def814cad698612a39edb7f40bca51ada
Eric Pan <epan@users.noreply.github.com>
2017-09-28 23:41:25 -0700

    (feat) Not working :( redux review

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,5 +1,5 @@
 import { SET_SEARCH_TERM } from './actions';
 
-export function setSearchTerm(searchTerm) {
-  return { type: SET_SEARCH_TERM, payload: searchTerm };
-}
+export const setSearchTerm = searchTerm => ({ type: SET_SEARCH_TERM, payload: searchTerm });
+
+export { setSearchTerm as default };
