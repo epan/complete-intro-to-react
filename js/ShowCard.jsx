4b83f97eefd284b877c6e13e50cd1233dd9de803
Eric Pan <epan@users.noreply.github.com>
2017-09-28 21:16:18 -0700

    (before switching to v3-19 for redux)

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -2,36 +2,35 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Wrapper = styled.div`
+const Wrapper = styled((Link: any))`
   width: 32%;
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
   width: 46%;
   float: left;
-  margin-right: 10px
+  margin-right: 10px;
 `;
 
-const ShowCard = (props: {
-  show: {
-    poster: string,
-    title: string,
-    year: string,
-    description: string
-  }
-}) => (
-  <Wrapper>
-    <Image src={`/public/img/posters/${props.show.poster}`} alt={`${props.show.title} Show Poster`} />
+const ShowCard = ({ show }: Show) => (
+  <Wrapper to={`/details/${show.imdbID}`}>
+    <Image
+      src={`/public/img/posters/${show.poster}`}
+      alt={`${show.title} Show Poster`}
+    />
     <div>
-      <h3>{props.show.title}</h3>
-      <h3>({props.show.year})</h3>
-      <p>{props.show.description}</p>
+      <h3>{show.title}</h3>
+      <h3>({show.year})</h3>
+      <p>{show.description}</p>
     </div>
   </Wrapper>
 );
